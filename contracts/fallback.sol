8547a8a2089d80d6408c8c1d5fcca62ec25c624c
zhuzhuxia480 <742491842@qq.com>
2024-09-21 22:06:37 +0800

    add fallback.sol

---
+++ contracts/fallback.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.26;
+
+contract Fallback{
+    event receivedCalled(address sender, uint value);
+    event fallbackCalled(address sender, uint value);
+
+    receive() external payable{
+        emit receivedCalled(msg.sender, msg.value);
+    }
+    
+    fallback() external payable{
+        emit fallbackCalled(msg.sender, msg.value);
+    }
+}
